c90a8f7de82a5e3de44889e928953e7ba4390d62
Malaprabha <malaprabhafarm@gmail.com>
2021-08-07 17:08:54 +0530

    fix path

--- js/app.js
+++ js/app.js
@@ -2,8 +2,3 @@ require('/css/style.css');
 require('/css/bootstrap.min.css');
 require('/css/fancybox.min.css');
 require('/js/main.js');
-// require('/js/jquery.min.js');
-// require('/js/popper.min.js');
-// require('/js/bootstrap.min.js');
-// require('/js/jquery-migrate-3.0.1.min.js');
-// require('/js/jquery.stellar.min.js');
