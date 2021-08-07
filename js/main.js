212fe1add8d6180fb4283fcca4611046cb5c7cb1
Malaprabha <malaprabhafarm@gmail.com>
2021-08-07 16:25:41 +0530

    Test Website structure

---
+++ js/main.js
@@ -0,0 +1,205 @@
+// aos
+AOS.init({
+  duration: 1000
+});
+
+var siteStellar = function () {
+  $(window).stellar({
+    responsive: false,
+    parallaxBackgrounds: true,
+    parallaxElements: true,
+    horizontalScrolling: false,
+    hideDistantElements: false,
+    scrollProperty: 'scroll'
+  });
+}
+siteStellar();
+
+var smoothScroll = function () {
+  var $root = $('html, body');
+
+  $('a.smoothscroll[href^="#"]').click(function () {
+    $root.animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+    return false;
+  });
+}
+smoothScroll();
+
+// :: Owl Carousel Active Code
+if ($.fn.owlCarousel) {
+
+  // home slider
+  $('.home-slider').owlCarousel({
+    loop: true,
+    autoplay: true,
+    margin: 10,
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    nav: true,
+    autoplayHoverPause: true,
+    items: 1,
+    autoheight: true,
+    navText: ["<span class='ion-chevron-left'></span>", "<span class='ion-chevron-right'></span>"],
+    responsive: {
+      0: {
+        items: 1,
+        nav: false
+      },
+      600: {
+        items: 1,
+        nav: false
+      },
+      1000: {
+        items: 1,
+        nav: true
+      }
+    }
+  });
+
+  $("[data-delay]").each(function () {
+    var anim_del = $(this).data('delay');
+    $(this).css('animation-delay', anim_del);
+  });
+
+  $("[data-duration]").each(function () {
+    var anim_dur = $(this).data('duration');
+    $(this).css('animation-duration', anim_dur);
+  });
+
+  $('.instagram-slides').owlCarousel({
+    items: 7,
+    margin: 0,
+    loop: true,
+    autoplay: true,
+    autoplayHoverPause: true,
+    autoplayTimeout: 2000, // Autoplay Timeout 1s = 1000ms
+    smartSpeed: 2000,
+    responsive: {
+      0: {
+        items: 2
+      },
+      480: {
+        items: 3
+      },
+      576: {
+        items: 4
+      },
+      992: {
+        items: 5
+      },
+      1500: {
+        items: 7
+      }
+    }
+  });
+
+  // owl carousel
+  var major2Carousel = $('.js-carousel-2');
+  major2Carousel.owlCarousel({
+    loop: true,
+    autoplay: true,
+    stagePadding: 7,
+    margin: 20,
+    // animateOut: 'fadeOut',
+    // animateIn: 'fadeIn',
+    nav: true,
+    autoplayHoverPause: true,
+    autoHeight: true,
+    items: 3,
+    navText: ["<span class='ion-chevron-left'></span>", "<span class='ion-chevron-right'></span>"],
+    responsive: {
+      0: {
+        items: 1,
+        nav: false
+      },
+      600: {
+        items: 2,
+        nav: false
+      },
+      1000: {
+        items: 3,
+        dots: true,
+        nav: true,
+        loop: false
+      }
+    }
+  });
+
+  /* 8. sildeBar scroll */
+  $.scrollUp({
+    scrollName: 'scrollUp', // Element ID
+    topDistance: '300', // Distance from top before showing element (px)
+    topSpeed: 300, // Speed back to top (ms)
+    animation: 'fade', // Fade, slide, none
+    animationInSpeed: 200, // Animation in speed (ms)
+    animationOutSpeed: 200, // Animation out speed (ms)
+    scrollText: '<i class="fa fa-angle-up fa-2x"></i>', // Text for element
+    activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+  });
+
+  // scroll
+  var scrollWindow = function () {
+    $(window).scroll(function () {
+      var $w = $(this),
+        st = $w.scrollTop(),
+        navbar = $('.ftco_navbar'),
+        sd = $('.js-scroll-wrap');
+
+      if (st > 150) {
+        if (!navbar.hasClass('scrolled')) {
+          navbar.addClass('scrolled');
+        }
+      }
+      if (st < 150) {
+        if (navbar.hasClass('scrolled')) {
+          navbar.removeClass('scrolled sleep');
+        }
+      }
+      if (st > 350) {
+        if (!navbar.hasClass('awake')) {
+          navbar.addClass('awake');
+        }
+
+        if (sd.length > 0) {
+          sd.addClass('sleep');
+        }
+      }
+      if (st < 350) {
+        if (navbar.hasClass('awake')) {
+          navbar.removeClass('awake');
+          navbar.addClass('sleep');
+        }
+        if (sd.length > 0) {
+          sd.removeClass('sleep');
+        }
+      }
+    });
+  };
+  scrollWindow();
+
+  	// navigation
+	var OnePageNav = function() {
+		$(".smoothscroll[href^='#'], #ftco-nav ul li a[href^='#']").on('click', function(e) {
+		 	e.preventDefault();
+
+		 	var hash = this.hash,
+		 			navToggler = $('.navbar-toggler');
+		 	$('html, body').animate({
+		    scrollTop: $(hash).offset().top
+		  }, 700, 'easeInOutExpo', function(){
+		    window.location.hash = hash;
+		  });
+
+
+		  if ( navToggler.is(':visible') ) {
+		  	navToggler.click();
+		  }
+		});
+		$('body').on('activate.bs.scrollspy', function () {
+		  console.log('nice');
+		})
+	};
+	OnePageNav();
+}
